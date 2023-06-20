c26cca9cf97fa9f2b5272df2a3cc8a764545f99c
Léia <leiaoliver388@gmail.com>
2023-06-20 14:18:31 -0300

    first commit

---
+++ Calculadora_python.py
@@ -0,0 +1,42 @@
+def soma(a, b):
+    return a + b
+
+
+def subtracao(a, b):
+    return a - b
+
+
+def divisao(a, b):
+    return a / b
+
+
+def multiplicacao(a, b):
+    return a * b
+
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print("Operações: ")
+print("1 - soma")
+print("2 - subtração")
+print("3 - Divisão")
+print("4 - multiplicação)")
+
+operacao = int(input("Digite o número da operação que irá realizar: "))
+
+
+resultado = 0
+
+if operacao == 1:
+    resultado = soma(num1, num2)
+elif operacao == 2:
+    resultado = subtracao(num1, num2)
+elif operacao == 3:
+    resultado = divisao(num1, num2)
+elif operacao == 4:
+    resultado = multiplicacao(num1, num2)
+else:
+    print("Operação inválida!")
+
+print(resultado)
